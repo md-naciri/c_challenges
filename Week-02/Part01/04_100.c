75f46264ed516f09a51b0ab6a08e1fe0a25f76d5
md_naciri <med.ar.naciri@gmail.com>
2022-09-12 17:29:17 +0100

    Update

--- Week-02/Part01/04_100.c
+++ Week-02/Part01/04_100.c
@@ -2,22 +2,22 @@
 #include<stdlib.h>
 int main()
 {
-    int sum,max,a;
+    int sum,M,n;
     sum=0;
-    max=0;
-    a=1;
-    printf("Entrez une serie d'entier, pour finir entrez 0.\n");
-    while(a!=0)
+    M=0;
+    n=1;
+    printf("Entrez une serie d'entier inferieur a 100\n");
+    while(n!=0)
     {
        do{
-            scanf("%d",&a);
-            if(a>100) printf("Entrez SVP un entier inferieur ou egal a 100\n");
-       }while(a>100);
-       sum+=a;
-       if(a>max) max=a;
+            scanf("%d",&n);
+            if(n>100) printf("Entrez un entier inferieur a 100\n");
+       }while(n>=100);
+       sum+=n;
+       if(n>M) M=n;
     }
     printf("la somme: %d\n",sum);
-    printf("le max: %d\n",max);
+    printf("le max: %d\n",M);
     
     return 0; 
 }
