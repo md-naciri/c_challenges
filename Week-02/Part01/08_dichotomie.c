c4bafe5cc3386d1769b7c92725eb07d0adc1d269
md_naciri <med.ar.naciri@gmail.com>
2022-09-13 17:32:52 +0100

    Correcting

--- Week-02/Part01/08_dichotomie.c
+++ Week-02/Part01/08_dichotomie.c
@@ -9,11 +9,11 @@ int main () {
     while ((b-a)>0.00001)
     {
         m=(b+a)/2;
-        if ((pow(a,3)+(12*pow(a,2))+1)*(pow(b,3)+(12*pow(b,2))+1)<0) b = m;
+        if ((pow(a,3)+(12*pow(a,2))+1)*(pow(m,3)+(12*pow(m,2))+1)<0) b = m;
         else a = m;
         
     }
-    printf("\nLa solution est: %.2f", m);
+    printf("\nLa solution est: %f", m);
     
     return 0;
 }
